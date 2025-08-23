794ec763a3d3e149bc290345ee4fd25f8430cb24
pedropaulo <pedropaulodecarlo@gmail.com>
2025-08-23 16:06:24 -0300

    Adiciona o CEP como dado.

--- DesafioMuralis/src/main/java/com/example/DesafioMuralis/model/Cliente.java
+++ DesafioMuralis/src/main/java/com/example/DesafioMuralis/model/Cliente.java
@@ -1,4 +1,5 @@
 package com.example.DesafioMuralis.model;
+
 import java.time.LocalDate;
 
 import org.hibernate.validator.constraints.br.CPF;
@@ -19,23 +20,33 @@ import jakarta.validation.constraints.Past;
 
 @Entity
 public class Cliente {
-    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
-    @NotBlank(message="O nome do cliente não pode estar vazio.") @Column(name="Nome", length=100, nullable=false)
+
+    @NotBlank(message = "O nome do cliente não pode estar vazio.")
+    @Column(name = "Nome", length = 100, nullable = false)
     private String nome;
 
-    @NotBlank(message="O CPF não pode estar vazio.")@CPF(message="CPF inválido.") @Column(name="CPF", length=14, nullable=false, unique=true)
+    @NotBlank(message = "O CPF não pode estar vazio.")
+    @CPF(message = "CPF inválido.")
+    @Column(name = "CPF", length = 14, nullable = false, unique = true)
     private String cpf;
 
-    @Past(message="A data de nascimento deve estar no passado.") @Column(name="Data de Nascimento", length=14, nullable=true, unique=false) @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") @JsonDeserialize(using = LocalDateDeserializer.class)
-@JsonSerialize(using = LocalDateSerializer.class)
-
+    @Past(message = "A data de nascimento deve estar no passado.")
+    @Column(name = "Data de Nascimento", length = 14, nullable = true, unique = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate data_nascimento;
 
-    @Column(name="Endereço", nullable=true)
+    @Column(name = "Endereço", nullable = true)
     private String endereco;
 
+    @Column(name = "CEP", length = 8, nullable = true)
+    private String cep;
+
+    // Getters e Setters
     public Long getId() {
         return id;
     }
@@ -75,5 +86,12 @@ public class Cliente {
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
-    
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 }
